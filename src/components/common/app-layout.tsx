de555c1370686361650e794de79b238c9482d25a
ohinhyuk <8156217@naver.com>
2025-06-18 23:21:04 +0900

    style: 모바일 뷰일 때 사이드바 트리거 추가

--- src/components/common/app-layout.tsx
+++ src/components/common/app-layout.tsx
@@ -1,4 +1,8 @@
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 import { CommonSidebar } from "./SideBar";
 import { Outlet } from "react-router-dom";
@@ -12,7 +16,8 @@ export default function Layout() {
       <CommonSidebar />
 
       <SidebarInset className="flex flex-col">
-        <main className="flex-1 p-6 py-0 bg-gradient-to-b from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 ">
+        <main className="flex-1 p-6 py-0 bg-gradient-to-b from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 relative">
+          <SidebarTrigger className="absolute top-2 left-4 block md:hidden" />
           <Outlet />
         </main>
       </SidebarInset>
