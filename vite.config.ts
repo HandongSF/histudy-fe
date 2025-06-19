89df46d2ae8b7fbd7b28f61e0f92a9571c8493de
Inhyuk Oh <49269218+ohinhyuk@users.noreply.github.com>
2025-06-20 01:37:46 +0900

    fix: 컴포넌트 props와 config 파일 타입 보완 (#124)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+// @ts-ignore
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
