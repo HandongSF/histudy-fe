8b9c939117ae2ce1af6a8077475f8705bc84e8aa
ohinhyuk <8156217@naver.com>
2025-07-06 23:44:52 +0900

    refactor: 사용하지 않는 전역 상태 삭제

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -7,12 +7,6 @@ const { persistAtom } = recoilPersist({
   storage: localStorage,
 });
 
-export const darkState = atom<boolean>({
-  key: "darkMode",
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
-
 export const isLoginState = atom<boolean>({
   key: "isLogin",
   default: false,
@@ -29,42 +23,9 @@ export const userLoginInfo = atom<JwtHIStudyPayload | null>({
   default: null,
 });
 
-export const isCodeModalState = atom<boolean>({
-  key: "codeModal",
-  default: false,
-});
-
-export const isDelete = atom<boolean>({
-  key: "deleteSnackbar",
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const selectState = atom<number>({
-  key: "sidebarSelect",
-  default: 1,
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const isLoadingState = atom<boolean>({
-  key: "isLoading",
-  default: false,
-});
-
-//TODO: 타입 정의 필요
-export const groupAutoCompleteState = atom<any[]>({
-  key: "groupAutoComplete",
-  default: [],
-});
-
 export type Role = "MEMBER" | "USER" | "ADMIN" | "NONUSER";
 
 export const roleState = atom<Role>({
   key: "role",
   default: "NONUSER",
-});
-
-export const isShowFullImageState = atom<boolean>({
-  key: "isShowFullImage",
-  default: false,
 });
