9a0166942f65180aec883323b307ea5edcfb32b1
ohinhyuk <8156217@naver.com>
2025-07-06 23:46:30 +0900

    refactor: Role 타입 interface 폴더로 이동

---
+++ src/interface/role.ts
@@ -0,0 +1,1 @@
+export type Role = "MEMBER" | "USER" | "ADMIN" | "NONUSER";
--- src/store/atom.ts
+++ src/store/atom.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { JwtHIStudyPayload } from "@/components/GoogleButton";
+import { Role } from "@/interface/role";
 
 const { persistAtom } = recoilPersist({
   key: "localStorage", //원하는 key 값 입력
@@ -22,8 +23,6 @@ export const userLoginInfo = atom<JwtHIStudyPayload | null>({
   key: "userLoginInfo",
   default: null,
 });
-
-export type Role = "MEMBER" | "USER" | "ADMIN" | "NONUSER";
 
 export const roleState = atom<Role>({
   key: "role",
