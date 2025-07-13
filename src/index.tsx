de7959e80d9f78d78331c288a7523ed41044401b
ohinhyuk <8156217@naver.com>
2025-07-13 22:18:57 +0900

    chore: recoil 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import * as ReactDOM from "react-dom/client";
 
-import { RecoilRoot } from "recoil";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
-);
+root.render(<App />);
 reportWebVitals(console.log);
