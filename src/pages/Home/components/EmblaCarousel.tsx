c4b228e06082e63a9d3bab2708cdb02b2a6b4c1a
Cocomong98 <yonghjjm0618@gmail.com>
2025-08-30 23:55:44 +0900

    캐러셀 기능 추가

---
+++ src/pages/Home/components/EmblaCarousel.tsx
@@ -0,0 +1,36 @@
+// src/pages/HomePage/components/EmblaCarousel.tsx
+
+import React from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import "./embla.css";
+
+type PropType = {
+    children: React.ReactNode;
+    options?: EmblaOptionsType;
+    className?: string;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+    const { children, options, className } = props;
+    const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+        Autoplay({ stopOnInteraction: false }),
+    ]);
+
+    return (
+        <section className={`embla ${className || ""}`}>
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    {React.Children.map(children, (child, index) => (
+                        <div className="embla__slide" key={index}>
+                            {child}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default EmblaCarousel;
