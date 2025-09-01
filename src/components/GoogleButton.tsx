b3bfda4e4d97115c003fa095b70e2a1891fc319e
Cocomong98 <yonghjjm0618@gmail.com>
2025-09-01 18:39:31 +0900

    g50901 : temp
    250801 : temp

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -12,82 +12,72 @@ import { userLogin } from "../apis/users";
 import { isRegisterModalState, userLoginInfoState } from "../store/HISAtom";
 import { useSidebar } from "@/components/ui/sidebar";
 
-
 export interface JwtHIStudyPayload extends JwtPayload {
-  hd: string;
-  rol: Role;
-  name: string;
-  email: string;
+    hd: string;
+    rol: Role;
+    name: string;
+    email: string;
 }
 
 export default function GoogleButton() {
-  // const role = useHIStateValue(roleState);
+    // const role = useHIStateValue(roleState);
 
-  const setIsRegisterModalState = useSetHiState(isRegisterModalState);
-  const setUserLoginInfo = useSetHiState(userLoginInfoState);
-  const { login } = useAuth();
+    const setIsRegisterModalState = useSetHiState(isRegisterModalState);
+    const setUserLoginInfo = useSetHiState(userLoginInfoState);
+    const { login } = useAuth();
 
-  // const { loginWithCredential } = useAuthContext();
-  const onSuccess = async (credentialResponse: CredentialResponse) => {
-    if (!credentialResponse.credential) {
-      toast.error("로그인에 실패하였습니다.");
-      return;
-    }
-    const decodedToken = jwtDecode(
-      credentialResponse.credential
-    ) as JwtHIStudyPayload;
-
-    if (
-      decodedToken.hd !== "handong.edu" &&
-      decodedToken.hd !== "handong.ac.kr"
-    ) {
-      toast.error("한동대학교 이메일로 로그인해주세요.");
-      return;
-    }
-
-    if (!decodedToken.sub) {
-      toast.error("로그인에 실패하였습니다.");
-      return;
-    }
-
-    // const testSub = {
-    //   ADMIN: "test3",
-    //   MEMBER: "test2",
-    //   USER: "test1",
-    // } as Record<Role, string>;
-
-    userLogin(
-      // testSub[role] ||
-      decodedToken.sub
-    )
-      .then((response) => {
-        if (response.isRegistered === true) {
-          login(
-            response.tokens.accessToken,
-            response.tokens.refreshToken,
-            response.role
-          );
+    // const { loginWithCredential } = useAuthContext();
+    const onSuccess = async (credentialResponse: CredentialResponse) => {
+        if (!credentialResponse.credential) {
+            toast.error("로그인에 실패하였습니다.");
+            return;
         }
-      })
-      // 구글 로그인 성공 후 히즈스터디 서버 로그인 API 에러 발생
-      .catch((error) => {
-        if (error.response && error.response.data.isRegistered === false) {
-          setIsRegisterModalState(true);
-          setUserLoginInfo(decodedToken);
+        const decodedToken = jwtDecode(credentialResponse.credential) as JwtHIStudyPayload;
+
+        if (decodedToken.hd !== "handong.edu" && decodedToken.hd !== "handong.ac.kr") {
+            toast.error("한동대학교 이메일로 로그인해주세요.");
+            return;
         }
-      });
-  };
 
-  const { state } = useSidebar();
+        if (!decodedToken.sub) {
+            toast.error("로그인에 실패하였습니다.");
+            return;
+        }
 
-  return (
-    <GoogleLogin
-      type={state === "collapsed" ? "icon" : "standard"}
-      onSuccess={(credentialResponse) => onSuccess(credentialResponse)}
-      onError={() => {
-        console.log("Login Failed");
-      }}
-      useOneTap
-    />
-  );
+        // const testSub = {
+        //   ADMIN: "test3",
+        //   MEMBER: "test2",
+        //   USER: "test1",
+        // } as Record<Role, string>;
+
+        userLogin(
+            // testSub[role] ||
+            decodedToken.sub
+        )
+            .then((response) => {
+                if (response.isRegistered === true) {
+                    login(response.tokens.accessToken, response.tokens.refreshToken, response.role);
+                }
+            })
+            // 구글 로그인 성공 후 히즈스터디 서버 로그인 API 에러 발생
+            .catch((error) => {
+                if (error.response && error.response.data.isRegistered === false) {
+                    setIsRegisterModalState(true);
+                    setUserLoginInfo(decodedToken);
+                }
+            });
+    };
+
+    const { state } = useSidebar();
+
+    return (
+        <GoogleLogin
+            type={state === "collapsed" ? "icon" : "standard"}
+            onSuccess={(credentialResponse) => onSuccess(credentialResponse)}
+            onError={() => {
+                console.log("Login Failed");
+            }}
+            useOneTap
+        />
+    );
 }
