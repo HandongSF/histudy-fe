cedf55753cfad1dc9df38125dca3b94c45cffd02
Cocomong98 <yonghjjm0618@gmail.com>
2025-09-07 02:04:46 +0900

    250905 : 배너 이미지 (슬기짜기 추가)

--- src/pages/Home/components/EmblaCarousel.tsx
+++ src/pages/Home/components/EmblaCarousel.tsx
@@ -4,43 +4,46 @@ import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 
 type PropType = {
-  children: React.ReactNode;
-  options?: EmblaOptionsType;
-  className?: string;
+    children: React.ReactNode;
+    options?: EmblaOptionsType;
+    className?: string;
 };
 
 export const CAROUSEL_SLIDES = [
-  {
-    imageUrl: "/img/SLE_DESKTOP.png",
-    linkUrl: "https://sle-recruitng.vercel.app/",
-    alt: "슬기짜기",
-  },
-  {
-    imageUrl: "/img/TEST_DESKTOP.png",
-    linkUrl: "https://hisnet.handong.edu/",
-    alt: "데모이미지",
-  },
+    {
+        imageUrl: "/img/SLE_Banner.png",
+        linkUrl: "https://sle-recruitng.vercel.app/",
+        alt: "슬기짜기",
+    },
+    {
+        imageUrl: "/img/TEST_DESKTOP.png",
+        linkUrl: "https://hisnet.handong.edu/",
+        alt: "데모이미지",
+    },
+    {
+        imageUrl: "/img/HISTUDY_Banner.png",
+        linkUrl: "https://docs.google.com/forms/d/1xnEGzFTi1AZ9xMefcUGlxSxvECbePaPAGjFn0QQSqag",
+        alt: "슬기짜기",
+    },
 ];
 
 const CAROUSEL_OPTIONS: EmblaOptionsType = { loop: true };
 
 export default function EmblaCarousel(props: PropType) {
-  const { children, className } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(CAROUSEL_OPTIONS, [
-    Autoplay({ stopOnInteraction: false }),
-  ]);
+    const { children, className } = props;
+    const [emblaRef, emblaApi] = useEmblaCarousel(CAROUSEL_OPTIONS, [Autoplay({ stopOnInteraction: false })]);
 
-  return (
-    <section className={`embla ${className || ""}`}>
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {React.Children.map(children, (child, index) => (
-            <div className="embla__slide" key={index}>
-              {child}
+    return (
+        <section className={`embla ${className || ""}`}>
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    {React.Children.map(children, (child, index) => (
+                        <div className="embla__slide" key={index}>
+                            {child}
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
